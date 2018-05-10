8a3d9a8b15aaadbaeb59bbf31c85d789d2634547
Mai Anh Kha <maianhkha.dev@gmail.com>
2018-05-10 18:19:41 +0700

    cms update

--- ecommerce/webpack.mix.js
+++ ecommerce/webpack.mix.js
@@ -11,5 +11,30 @@ let mix = require('laravel-mix');
  |
  */
 
+ mix.browserSync('127.0.0.1:8000')
+
+mix.webpackConfig({
+ module: {
+   rules: [{
+     test: /\.(png|jpe?g|gif)$/,
+     loader: 'file-loader',
+     options: {
+         name: 'images/[name].[hash].[ext]',
+         publicPath: '/'
+     }
+   }]
+ }
+})
+
+// mix.js('resources/assets/js/app.js', 'public/js').extract(['vue'])
+// mix.sass('resources/assets/scss/app.scss', 'public/css')
+// mix.sass('resources/assets/scss/layout.default.scss', 'public/css')
+// mix.sass('resources/assets/scss/pages.login.scss', 'public/css')
+// mix.sass('resources/assets/scss/home.scss', 'public/css')
+// mix.sass('resources/assets/scss/pages.index.scss', 'public/css')
+// mix.sass('resources/assets/scss/pages.show.scss', 'public/css')
+// mix.sass('resources/assets/scss/pages.edit.scss', 'public/css')
+// mix.sass('resources/assets/scss/tasks.create.scss', 'public/css')
+
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
